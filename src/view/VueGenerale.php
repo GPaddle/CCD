cbfd52a9b0769554633c1c8a4c8f8b622ad4f7dd
Keller Guillaume <keller.guillaume68@gmail.com>
2020-02-13 14:18:47 +0100

    tmp style

--- src/view/VueGenerale.php
+++ src/view/VueGenerale.php
@@ -6,6 +6,11 @@ class VueGenerale
 {
 	function header()
 	{
+
+		
+        $app = \Slim\Slim::getInstance();
+        $urlHome = $app->urlFor('route_home');
+
 		$html = <<<END
 		
 		<meta charset="utf-8">
@@ -17,16 +22,9 @@ class VueGenerale
 		<title>Gestion personnel</title>
 	  
 		<!-- Custom fonts for this template-->
-		<link href="style/style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="$urlHome/style/style.css">
+		<link href="$urlHome" rel="stylesheet" type="text/css">
 		<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-END;
-		return ($html);
-	}
-
-	function footer()
-	{
-		$html = <<<END
-		<footer></footer>
 END;
 		return ($html);
 	}
@@ -48,6 +46,16 @@ END;
 		return $html;
 	}
 
+	function footer()
+	{
+		$html = <<<END
+		<footer>
+		2019-2020 @IUT NC
+		</footer>
+END;
+		return ($html);
+	}
+
 
 	public function render($html)
 	{
@@ -62,7 +70,9 @@ END;
 		$header
 		<body>
 			$navBarre
-			$html
+			<section class="content">
+				$html
+			</section>
 			$footer
 
 
