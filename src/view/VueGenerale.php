874213e7bf5340e8ccaf9abc66239a5dbdd07883
martin <martin.jossic@outlook.fr>
2020-02-13 16:26:03 +0100

    Suppression des liens vers les fichiers JavaScript manquant

--- src/view/VueGenerale.php
+++ src/view/VueGenerale.php
@@ -136,20 +136,13 @@ END;
 		$app = \Slim\Slim::getInstance();
 		$urlHome = $app->urlFor('route_home');
 
+    if($urlHome == "/") {
+      $urlHome = "";
+    }
 
 		$html = <<<END
-    <!-- Bootstrap core JavaScript-->
-  <script src="$urlHome/vendor/jquery/jquery.min.js"></script>
-  <script src="$urlHome/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="$urlHome/vendor/jquery-easing/jquery.easing.min.js"></script>
-
   <!-- Custom scripts for all pages-->
   <script src="$urlHome/js/sb-admin-2.min.js"></script>
-
-  <!-- Page level plugins -->
-  <script src="$urlHome/vendor/chart.js/Chart.min.js"></script>
 
   <!-- Page level custom scripts -->
   <script src="$urlHome/js/demo/chart-area-demo.js"></script>
