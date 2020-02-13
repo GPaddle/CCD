7a904da1501746a4a8a3345faf8adbad9422d673
Keller Guillaume <keller.guillaume68@gmail.com>
2020-02-13 17:51:08 +0100

    Customisation de la page d'accueil

--- src/controler/HomeControler.php
+++ src/controler/HomeControler.php
@@ -10,15 +10,23 @@ use GEG\model\Creneau;
 use GEG\view\VuePrincipale;
 use GEG\view\VueGenerale;
 
-class HomeControler {
-    public function render($app) {
-    	if(isset($_SESSION["user"])) {
-    		$t = Creneau::orderBy("cycle")->orderBy("semaine")->orderBy("jour")->orderBy("debutHeure")->get();
-		    $v = new VuePrincipale($t);
-		    $v->render();
-    	} else {
-    		$vGenerale = new VueGenerale();
-    		$vGenerale->render("<a href={$app->urlFor('connexion')}>Connexion à la page</a>");
-    	}
-    }
+class HomeControler
+{
+	public function render()
+	{
+		$app = \Slim\Slim::getInstance();
+		if (isset($_SESSION["user"])) {
+			$t = Creneau::orderBy("cycle")->orderBy("semaine")->orderBy("jour")->orderBy("debutHeure")->get();
+			$v = new VuePrincipale($t);
+			$v->render();
+		} else {
+			$vGenerale = new VueGenerale();
+
+			$urlModif = $app->urlFor('connexion');
+			$urlModif2 = $app->urlFor('inscrire');
+			$urlImg = $app->urlFor('route_home');
+
+			$vGenerale->render("<img style='width:30vh' src='{$urlImg}img/logo.svg'></img><br><a href={$urlModif}>Connexion à la page</a><br><a href={$urlModif2}>S'inscrire à la page</a>");
+		}
+	}
 }
