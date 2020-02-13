16eb77da234c737e473125f141c4cc1884d78001
nicolas <nicolas.baudon21@gmail.com>
2020-02-13 14:01:13 +0100

    vue de test

--- src/controler/testControler.php
+++ src/controler/testControler.php
@@ -5,6 +5,7 @@ namespace GEG\controler;
 use \Illuminate\Database\Capsule\Manager as DB;
 use GEG\model\Liste;
 use GEG\model\Item;
+use GEG\model\Creneau;
 use GEG\view\VuePrincipaleTest;
 
 
@@ -12,7 +13,8 @@ class testControler
 {
     public function afficher()
     {
-        $v = new VuePrincipaleTest();
-        $v->render();
+      $t = Creneau::all();
+      $v = new VuePrincipaleTest($t);
+      $v->render();
     }
 }
--- src/view/VuePrincipaleTest.php
+++ src/view/VuePrincipaleTest.php
@@ -3,13 +3,22 @@
 namespace GEG\view;
 class VuePrincipaleTest
 {
+
+  private $t;
+
+  public function __construct($tab){
+    $this->t = $tab;
+    printf($this->t);
+  }
+
+
 	public function render()
 	{
 		$vGenerale = new VueGenerale();
 
 		$vGenerale->render(
 			"<div id='content'>
-      <div id='A' class='semaine'>
+      <div id='A' class='semaine' style='background-colot:grey;'>
         <div class='jour'></div>
         <div class='jour'></div>
         <div class='jour'></div>
